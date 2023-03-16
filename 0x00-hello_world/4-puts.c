01c479cef5ebe106c2c31781666ef659724a48bf
mohammed zeroual <mohammed.zeroual.mz@gmail.com>
2023-03-16 15:06:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include "stdio.h"
-int main()
+/*
+ 	print message using puts function in stdio.h
+*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
